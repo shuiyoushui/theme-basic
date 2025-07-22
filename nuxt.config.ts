34a07acaebb0d213d7f65134d9b0ee5dd73b9f9c
shuiyoushui <yihui352578739@gmail.com>
2025-07-22 23:42:12 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+    app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap'
+        }
+      ]
+    }
+  },
+  
   runtimeConfig: {
     idatariverMerchantSecret: '',
     public: {
