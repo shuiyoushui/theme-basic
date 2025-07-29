a0af4689aa3e3a46fed128d5b9418629fc1cd287
shuiyoushui <yihui352578739@gmail.com>
2025-07-29 12:27:01 +0000

    update: 修改 nuxt plugin 客服插件引入的配置代码

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,11 +15,7 @@ export default defineNuxtConfig({
     }
   },
 
-  plugins: [
-    '~/plugins/gtag.client.ts', // ✅ 引入 GA 插件
-    '~/plugins/chaport.client.ts' 
-
-  ],
+ 
 
   runtimeConfig: {
     idatariverMerchantSecret: '',
