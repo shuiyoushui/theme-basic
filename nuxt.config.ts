9e3dcc0dcf137b83c2513e64d6cc84843dc8c33b
shuiyoushui <yihui352578739@gmail.com>
2025-07-29 13:00:02 +0000

    update: 修改 nuxt plugin 客服插件引入的配置代码

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  
+runtimeConfig: {
+    public: {
+      baseURL: process.env.BASE_URL || 'https://hub.cryptoboost.vip'
+    }
+  },
 
   app: {
     head: {
